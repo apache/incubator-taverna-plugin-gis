96aea111e87260c66fa7831fcb90d4667dce120d
Edi Karadumi <edikaradumi@gmail.com>
2016-07-12 22:34:29 +0100

    implemented gis-client inteface and corresponding JUnit tests

--- taverna-gis-client/src/main/java/org/apache/taverna/gis/client/impl/NorthClientImpl.java
+++ taverna-gis-client/src/main/java/org/apache/taverna/gis/client/impl/NorthClientImpl.java
@@ -20,29 +20,113 @@
  */
 package org.apache.taverna.gis.client.impl;
 
+import java.io.IOException;
+import java.math.BigInteger;
 import java.net.URI;
 import java.util.HashMap;
 
 import org.apache.taverna.gis.client.IGisClient;
+import org.n52.wps.client.WPSClientException;
+import org.n52.wps.client.WPSClientSession;
+
+import net.opengis.ows.x11.LanguageStringType;
+import net.opengis.wps.x100.CapabilitiesDocument;
+import net.opengis.wps.x100.InputDescriptionType;
+import net.opengis.wps.x100.OutputDescriptionType;
+import net.opengis.wps.x100.ProcessDescriptionType;
 
 public class NorthClientImpl implements IGisClient {
 
 	@Override
 	public String GetServiceCapabilities(URI serviceURI) {
-		// TODO Auto-generated method stub
-		return null;
+		WPSClientSession wpsClient = WPSClientSession.getInstance();
+
+		try {
+			wpsClient.connect(serviceURI.toString());
+		} catch (WPSClientException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		CapabilitiesDocument capabilities = wpsClient.getWPSCaps(serviceURI.toString());
+
+		LanguageStringType[] serviceAbstract = capabilities.getCapabilities().getServiceIdentification()
+				.getTitleArray();
+		//
+		// ProcessBriefType[] processList = capabilities.getCapabilities()
+		// .getProcessOfferings().getProcessArray();
+		//
+		// for (ProcessBriefType process : processList) {
+		// System.out.println(process.getIdentifier().getStringValue());
+		// }
+		// return capabilities;
+		if (serviceAbstract != null && serviceAbstract.length > 0)
+			return serviceAbstract[0].getStringValue();
+		else
+			return null;
 	}
 
 	@Override
 	public HashMap<String, Integer> GetProcessInputPorts(URI serviceURI, String processID) {
-		// TODO Auto-generated method stub
-		return null;
+		HashMap<String, Integer> inputPorts = new HashMap<String, Integer>();
+		
+		WPSClientSession wpsClient = WPSClientSession.getInstance();
+
+		ProcessDescriptionType processDescription = null;
+		
+		try {
+			processDescription = wpsClient.getProcessDescription(serviceURI.toString(), processID);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (processDescription==null)
+			return null;
+		
+		InputDescriptionType[] inputList = processDescription.getDataInputs().getInputArray();
+
+		for (InputDescriptionType input : inputList) {
+
+			// if compareTo returns 1 then first value is greater than 1. it means that there is more than one occurrence therefore the depth is more than 0
+			int depth = ((input.getMaxOccurs().compareTo(BigInteger.valueOf(1))==1) ? 1 : 0);
+			
+			inputPorts.put(input.getIdentifier().getStringValue(), depth);
+		}
+		
+		return inputPorts;
+		
 	}
 
 	@Override
 	public HashMap<String, Integer> GetProcessOutputPorts(URI serviceURI, String processID) {
-		// TODO Auto-generated method stub
-		return null;
+		HashMap<String, Integer> outputPorts = new HashMap<String, Integer>();
+		
+		WPSClientSession wpsClient = WPSClientSession.getInstance();
+
+		ProcessDescriptionType processDescription = null;
+		
+		try {
+			processDescription = wpsClient.getProcessDescription(serviceURI.toString(), processID);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (processDescription==null)
+			return null;
+		
+		OutputDescriptionType[] outputList = processDescription.getProcessOutputs().getOutputArray();
+
+		for (OutputDescriptionType output : outputList) {
+
+			// TODO: Calculate output depth
+			int depth = 0;
+			
+			outputPorts.put(output.getIdentifier().getStringValue(), depth);
+		}
+		
+		return outputPorts;
 	}
 
 }
---
+++ taverna-gis-client/src/test/java/org/apache/taverna/gis/client/impl/NorthClientImplTest.java
@@ -0,0 +1,65 @@
+package org.apache.taverna.gis.client.impl;
+
+import static org.junit.Assert.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+public class NorthClientImplTest {
+
+	@Test
+	public void testGetServiceCapabilities() throws URISyntaxException {
+
+		URI uri = new URI("http://localhost:8080/geoserver/ows");
+	
+		String result = new NorthClientImpl().GetServiceCapabilities(uri);
+		
+		assertEquals("Incorrect service title",result, "Prototype GeoServer WPS");
+		
+	}
+	
+	@Test
+	public void testGetProcessInputPorts() throws URISyntaxException {
+
+		URI uri = new URI("http://localhost:8080/geoserver/ows");
+		
+		String processID = "gs:StringConcatWPS";
+	
+		HashMap<String, Integer> expectedInputResult = new HashMap<String, Integer>();
+		expectedInputResult.put("name", 0);
+		expectedInputResult.put("surname", 0);
+		
+		HashMap<String,Integer> result = new NorthClientImpl().GetProcessInputPorts(uri, processID);
+		
+		assertEquals("Incorrect input ports result",result, expectedInputResult);
+		
+	}
+	
+	@Test
+	public void testGetProcessOutputPorts() throws URISyntaxException {
+
+		URI uri = new URI("http://localhost:8080/geoserver/ows");
+		
+		String processID = "gs:StringConcatWPS";
+	
+		// check if the ports match
+		HashMap<String, Integer> expectedOutputResult = new HashMap<String, Integer>();
+		expectedOutputResult.put("result", 0);
+		
+		HashMap<String,Integer> result = new NorthClientImpl().GetProcessOutputPorts(uri, processID);
+		
+		// check if the ports do not match
+		assertEquals("Incorrect output ports result",result, expectedOutputResult);
+		
+		expectedOutputResult.clear();
+		expectedOutputResult.put("result1", 0);
+		
+		assertNotEquals("The output ports result should not match", result, expectedOutputResult);
+		
+	}
+	
+
+}
